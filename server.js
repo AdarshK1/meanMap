e5f76205a507d1af99d65fc2577c590a50a10f93
AdarshK1 <addy2999@gmail.com>
2015-12-05 18:03:44 -0500

    asf

--- server.js
+++ server.js
@@ -1,45 +1,33 @@
-(function() {
-  var app, bodyParser, express, methodOverride, mongoose, morgan, port;
+// Dependencies
+// -----------------------------------------------------
+var express         = require('express');
+var mongoose        = require('mongoose');
+var port            = process.env.PORT || 3000;
+var morgan          = require('morgan');
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
+var app             = express();
 
-  express = require('express');
+// Express Configuration
+// -----------------------------------------------------
+// Sets the connection to MongoDB
+mongoose.connect("mongodb://localhost/MeanMapApp");
 
-  mongoose = require('mongoose');
+// Logging and Parsing
+app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
+app.use('/bower_components',  express.static(__dirname + '/bower_components')); // Use BowerComponents
+app.use(morgan('dev'));                                         // log with Morgan
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
+app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+app.use(methodOverride());
 
-  port = process.env.port || 3000;
+// Routes
+// ------------------------------------------------------
+require('app/routes.js');
 
-  morgan = require('morgan');
-
-  bodyParser = require('body-parser');
-
-  methodOverride = require('method-override');
-
-  app = express();
-
-  mongoose.connect("mongodb://localhost/MeanMapApp");
-
-  app.use(express["static"](__dirname + '/public'));
-
-  app.use('/bower_components', express["static"](__dirname + '/bower_components'));
-
-  app.use(morgan('dev'));
-
-  app.use(bodyParser.json());
-
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-
-  app.use(bodyParser.text());
-
-  app.use(bodyParser.json({
-    type: 'application/vnd.api+json'
-  }));
-
-  app.use(methodOverride());
-
-  app.listen(port);
-
-  console.log('App listening on port ' + port);
-
-}).call(this);
-
+// Listen
+// -------------------------------------------------------
+app.listen(port);
+console.log('App listening on port ' + port);
